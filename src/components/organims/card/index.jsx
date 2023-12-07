8d65c8459c81c71a70dcf095662c0181e459f7a4
crontreras1 <crontreras1@gmail.com>
2023-12-06 22:51:24 -0500

    cards styles and logic Done

--- src/components/organims/card/index.jsx
+++ src/components/organims/card/index.jsx
@@ -2,25 +2,32 @@ import { UserCircleIcon } from '@heroicons/react/24/solid'
 import { Link } from 'react-router-dom'
 
 
+
 function Card (data) {
     console.log(data.data.id);
     return (
         <Link to={ `profile/${data.data.id_profile}` }>
-            <div className='cursor-pointer w-96 h-96 p-3 sm:ring-1 sm:ring-inset sm:ring-gray-500/10'>
-                <figure className='h-1/2'>
-                    { data.data.avatar ? <img
-                        src={ data.data.avatar }
-                        alt="" 
-                        className='w-full h-full rounded-xl object-cover'
-                    /> : < UserCircleIcon />}
-                </figure>
-                <p className="text-2xl px-2.5 font-semibold leading-7 text-gray-900">{ `${data.data.first_name} ${data.data.last_name}` }</p>
-                <p className="text-sm px-2.5 font-medium leading-6 text-gray-600">{ data.data.rol }</p>
-                <p className="text-sm px-2.5 leading-6 text-gray-600">{ data.data.location }</p>
-                <p className="text-sm px-2.5 leading-6 text-gray-900">{ data.data.benefits }</p>
-                <div className="flex justify-evenly items-center text-sm">
-                    <span className="text-sm px-2.5 font-medium leading-6 text-gray-600">{ `$${data.data.min_price} - $${data.data.max_price}` }</span>
-                    <p className="p-1 rounded-full flex justify-center w-32 text-sm font-bold leading-6 text-focus bg-focus/40">{ data.data.free_class  }</p>
+            <div className='cursor-pointer w-72 h-96 p-3 ring-1 ring-inset ring-gray-500/10'>
+                <div className='h-1/2 relative'>
+                    <figure className='w-full h-full'>
+                        { data.data.avatar ? <img
+                            src={ data.data.avatar }
+                            alt="" 
+                            className='w-full h-full rounded-xl object-cover'
+                        /> : < UserCircleIcon className='w-full h-full object-cover' /> }
+                    </figure>
+
+                    { data.data.rol ? <p className="text-xs absolute left-2 bottom-2 font-medium leading-6 rounded-md bg-gray-50 px-2 py-0 text-gray-600 ring-1 ring-inset ring-gray-500/10">{ data.data.rol }</p> : <span className='hidden'></span> }
+                </div>
+
+                <div className='h-1/2 flex flex-col justify-evenly'>
+                    { data.data.first_name ? <p className="text-2xl px-2.5 font-semibold leading-7 text-gray-900">{ `${data.data.first_name} ${data.data.last_name}` }</p> : <span className='hidden' ></span> }
+                    { data.data.location ? <p className="text-sm px-2.5 leading-6 text-gray-600">{ data.data.location }</p> : <span className='hidden'></span> }
+                    { data.data.benefits ? <p className="text-xs px-2.5 leading-6 text-gray-900">{ data.data.benefits }</p> : <span className='hidden'></span> }
+                    <div className="flex justify-evenly items-center text-sm">
+                        { data.data.min_price && data.data.max_price ? <span className="text-sm px-2.5 font-medium leading-6 text-gray-600">{ `$ ${data.data.min_price} - $ ${data.data.max_price}` }</span> : <span className='hidden'></span> }
+                        { data.data.free_class ? <p className="p-0 rounded-full flex justify-center w-24 text-xs font-bold leading-6 text-focus bg-focus/40">1ra Clase Gratis</p> : <span className='hidden'></span> }
+                    </div>
                 </div>
             </div>
         </Link>
