dc9d0ef782360904dd614ba22a0e7ff641232393
crontreras1 <crontreras1@gmail.com>
2023-12-23 13:45:22 -0500

    fix profile page styles

--- src/components/organims/card/index.jsx
+++ src/components/organims/card/index.jsx
@@ -20,7 +20,7 @@ function Card (data) {
                 </div>
 
                 <div className='h-1/2 flex flex-col justify-evenly'>
-                    { data.data.first_name ? <p className="text-2xl px-2.5 font-semibold leading-7 text-gray-900">{ `${data.data.first_name} ${data.data.last_name}` }</p> : <span className='hidden' ></span> }
+                    { data.data.first_name ? <p className="text-xl px-2.5 font-semibold leading-7 text-gray-900">{ `${data.data.first_name} ${data.data.last_name}` }</p> : <span className='hidden' ></span> }
                     
                     { data.data.location ? <p className="text-sm px-2.5 leading-6 text-gray-600">{ data.data.location }</p> : <span className='hidden'></span> }
                     
@@ -29,7 +29,7 @@ function Card (data) {
                     <div className="flex justify-evenly items-center text-sm">
                         { data.data.min_price && data.data.max_price ? <span className="text-sm px-2.5 font-medium leading-6 text-gray-600">{ `$ ${data.data.min_price} - $ ${data.data.max_price}` }</span> : <span className='hidden'></span> }
                         
-                        { data.data.free_class ? <p className="p-0 flex justify-center w-24 text-xs font-bold leading-6 text-buttonLink">1ra Clase Gratis</p> : <span className='hidden'></span> }
+                        { data.data.free_class ? <p className="p-0 flex justify-center w-24 text-xs font-bold leading-6 text-primaryColor">1ra Clase Gratis</p> : <span className='hidden'></span> }
                     </div>
                 </div>
             </div>
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -85,7 +85,7 @@ function Profile () {
                         <div className='col-span-2 p-4'>
                             <div className="mt-6 col-span-full">
                                 {/* // benefits */}
-                                <h2 className="mb-3 text-2xl font-bold leading-9 tracking-tight text-gray-900">{ profileData && profileData.benefits }</h2>
+                                <h2 className="mb-3 text-3xl font-bold leading-9 tracking-tight text-gray-900 lg:text-5xl">{ profileData && profileData.benefits }</h2>
                                 <div className="flex w-full gap-3 flex-wrap">
                                     {/* faceToFaceClasses */}
                                     { profileData && profileData.face_to_face_classes ? <span className="w-auto inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Presencial</span> : <span className='hidden'></span> }
@@ -102,25 +102,25 @@ function Profile () {
 
                                 <div className=" mt-6 col-span-full">
                                     {/* // biography */}
-                                    <p className="block text-sm font-medium leading-6 text-gray-900">Acerca de mi</p>
-                                    <p className="mb-3 text-sm leading-6 text-gray-600">{ profileData && profileData.biography }</p>
+                                    <p className="block text-xl font-medium leading-6 text-gray-900">Acerca de mi</p>
+                                    <p className="mb-3 text-xl leading-6 text-gray-600">{ profileData && profileData.biography }</p>
                                 </div>
 
                                 <div className=" mt-6 col-span-full">
                                     {/* // education */}
-                                    <p className="block text-sm font-medium leading-6 text-gray-900">Logros académicos</p>
-                                    <p className="mb-3 text-sm leading-6 text-gray-600">{profileData && profileData.education }</p>
+                                    <p className="block text-xl font-medium leading-6 text-gray-900">Logros académicos</p>
+                                    <p className="mb-3 text-xl leading-6 text-gray-600">{profileData && profileData.education }</p>
                                 </div>
 
                                 <div className=" mt-6 col-span-full">
                                     {/* schedule */}
-                                    <p htmlFor="schedule" className="block text-sm font-medium leading-6 text-gray-900">Agenda:</p>
+                                    <p htmlFor="schedule" className="block text-xl font-medium leading-6 text-gray-900">Agenda:</p>
 
-                                    <p className="mb-3 text-sm leading-6 text-gray-600">{profileData && profileData.schedule }</p>
+                                    <p className="mb-3 text-xl leading-6 text-gray-600">{profileData && profileData.schedule }</p>
                                 </div>
 
                                 <div className='my-8'>
-                                    <button className='rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 w-full flex justify-center items-center gap-2 sm:w-1/2'>{ <MdOutlineReport  className='w-5 h-5' /> }Reportar Perfil</button>
+                                    <button className='rounded-md bg-white px-2.5 py-1.5 text-xl font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 w-full flex justify-center items-center gap-2 sm:w-1/2'>{ <MdOutlineReport  className='w-5 h-5' /> }Reportar Perfil</button>
                                 </div>
                             </div>
                         </div>
@@ -133,19 +133,19 @@ function Profile () {
                             <div className="mt-6 gap-y-8 sm:grid-cols-6">
                                 <div className="sm:col-span-3">
                                     {/* // FirtsName and SecondName */}
-                                    <p htmlFor="firstName" className="text-center text-sm font-medium leading-6 text-gray-900">{ profileData && profileData.first_name }  { profileData && profileData.last_name }</p>
+                                    <p htmlFor="firstName" className="text-center text-xl font-medium leading-6 text-gray-900">{ profileData && profileData.first_name }  { profileData && profileData.last_name }</p>
                                     
                                     {/* Rol */}
-                                    <p htmlFor="firstName" className="text-center mb-3 text-sm leading-6 text-gray-600">{ profileData && profileData.rol }</p>
+                                    <p htmlFor="firstName" className="text-center mb-3 text-xl leading-6 text-gray-600">{ profileData && profileData.rol }</p>
                                     {/* minPrice and MaxPrice */}
-                                    <p htmlFor="firstName" className="text-center mb-3 text-sm leading-6 text-gray-600">Valor de la clase:<br />{`$${profileData && profileData.min_price} - $${profileData && profileData.max_price}` }</p>
+                                    <p htmlFor="firstName" className="text-center mb-3 text-xl leading-6 text-gray-600">Valor de la clase:<br />{`$${profileData && profileData.min_price} - $${profileData && profileData.max_price}` }</p>
                                 </div>
                             </div>
 
                             <div className="w-full mt-6 flex justify-center sm:col-span-4">
                                 { 
                                     profileData && profileData.tel ? 
-                                        <button onClick={ openContactModalBtn } className="w-full rounded-md cursor-pointer bg-lime-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 lg:w-2/3rounded-md  flex justify-center items-center md:w-full gap-2 sm:w-1/2 lg:w-3/5">
+                                        <button onClick={ openContactModalBtn } className="w-full rounded-md cursor-pointer bg-lime-600 px-2.5 py-1.5 text-xl font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 lg:w-2/3rounded-md  flex justify-center items-center md:w-full gap-2 sm:w-1/2 lg:w-3/5">
                                             < FaWhatsapp className='text-white w-5 h-5' />
 
                                             Contactar
